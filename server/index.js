e24406ab2126f18b29449baa24eaa1ac480aff0b
DikshakAdhikari <ddikshakk@gmail.com>
2023-08-27 03:57:56 +0530

    gip

--- server/index.js
+++ server/index.js
@@ -12,16 +12,13 @@ const allowedOrigins = [
     // Add more allowed origins if needed
   ];
   
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }
-  }));
-
+  const cors = require('cors');
+  const corsOptions ={
+      origin:allowedOrigins, 
+      credentials:true,            //access-control-allow-credentials:true
+      optionSuccessStatus:200
+  }
+  app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/admin", adminRouter)
