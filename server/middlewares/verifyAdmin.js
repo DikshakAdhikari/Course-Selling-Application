f232fdb646486f8f513a4624a5a9ed44afcf346e
DikshakAdhikari <ddikshakk@gmail.com>
2023-08-27 03:08:51 +0530

    deploy10

--- server/middlewares/verifyAdmin.js
+++ server/middlewares/verifyAdmin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt= require('jsonwebtoken');
+
 const authenticateJwt = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (authHeader) {
