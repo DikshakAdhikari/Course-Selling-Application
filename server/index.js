c9571e8a54dd9029ded50b86e826745c46891e46
DikshakAdhikari <ddikshakk@gmail.com>
2023-08-27 03:26:34 +0530

    dep1

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.get("/", (req, res)=>{
   res.json('Server is Live');
 })
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" })
  .then(()=>console.log('database connected'))
  .catch((err)=>console.log(err))
 
